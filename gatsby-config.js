23c5c9efa54c4e8ddf35e18dc9dc00cb975984bf
max80713 <max80713@hotmail.com>
2022-06-11 09:14:45 +0800

    Update site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Blog`,
+    description: `Some thoughts`,
+    author: `max80713`,
+    siteUrl: `https://max80713githubiomain.gtsb.io/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
